f5ca8cce128f459ad82ec138789050054ca57bc7
Muhammad Akmal <makmal316@gmail.com>
2025-04-12 11:32:35 +0700

    fix: restructure Menu resource form with grouped sections and improved layout

--- app/Filament/Resources/MenuResource.php
+++ app/Filament/Resources/MenuResource.php
@@ -26,88 +26,106 @@ class MenuResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
+            ->columns(3)
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->label('Nama')
-                    ->required()
-                    ->maxLength(100),
-                Forms\Components\Select::make('category')
-                    ->label('Kategori')
-                    ->options(MenuCategory::select())
-                    ->live()
-                    ->required(),
-                Forms\Components\Textarea::make('description')
-                    ->label('Deskripsi')
-                    ->hint('Opsional')
-                    ->columnSpanFull(),
-                Forms\Components\FileUpload::make('image')
-                    ->label('Gambar')
-                    ->image()
-                    ->required()
-                    ->columnSpanFull(),
-                Forms\Components\Checkbox::make('has_variant')
-                    ->label('Memiliki Varian')
-                    ->live()
-                    ->helperText('Centang jika menu ini memiliki varian (Hot & Cold)')
-                    ->visible(
-                        fn(Get $get): bool => $get('category') == MenuCategory::Beverage->value
-                    )
-                    ->columnSpanFull()
-                    ->afterStateHydrated(function (?Menu $record, Set $set, string $operation): void {
-                        if ($operation === 'edit' && $record->prices->count() > 1) {
-                            $set('has_variant', true);
-                        }
-                    }),
-                Forms\Components\TextInput::make('price')
-                    ->label(function (Get $get): string {
-                        $isBeverage = $get('category') == MenuCategory::Beverage->value;
-                        return $get('has_variant') && $isBeverage
-                            ? 'Harga (Hot)'
-                            : 'Harga';
-                    })
-                    ->required()
-                    ->numeric()
-                    ->mask(RawJs::make("\$money(\$input, ',', '.')"))
-                    ->stripCharacters('.')
-                    ->prefix('Rp')
-                    ->afterStateHydrated(function (Set $set, ?Menu $record, string $operation): void {
-                        if ($operation === 'edit') {
-                            $prices = $record->prices;
-                            if ($record->prices->count() > 1) {
-                                $set(
-                                    'price',
-                                    $prices->firstWhere(
-                                        'variant_beverage',
-                                        VariantBeverage::Hot
-                                    )->price
-                                );
+                Forms\Components\Group::make([
+                    Forms\Components\Section::make()
+                        ->columns()
+                        ->schema([
+                            Forms\Components\TextInput::make('name')
+                                ->label('Nama')
+                                ->required()
+                                ->maxLength(100),
+                            Forms\Components\Select::make('category')
+                                ->label('Kategori')
+                                ->options(MenuCategory::select())
+                                ->live()
+                                ->required(),
+                            Forms\Components\Checkbox::make('has_variant')
+                                ->label('Memiliki Varian')
+                                ->live()
+                                ->helperText('Centang jika menu ini memiliki varian (Hot & Cold)')
+                                ->visible(
+                                    fn(Get $get): bool => $get('category') == MenuCategory::Beverage->value
+                                )
+                                ->columnSpanFull()
+                                ->afterStateHydrated(function (?Menu $record, Set $set, string $operation): void {
+                                    if ($operation === 'edit' && $record->prices->count() > 1) {
+                                        $set('has_variant', true);
+                                    }
+                                }),
+                            Forms\Components\Textarea::make('description')
+                                ->label('Deskripsi')
+                                ->hint('Opsional')
+                                ->columnSpanFull(),
+                        ]),
+                    Forms\Components\Section::make()
+                        ->schema([
+                            Forms\Components\FileUpload::make('image')
+                                ->label('Gambar')
+                                ->image()
+                                ->required()
+                                ->columnSpanFull(),
+                        ]),
+                ])->columnSpan(2),
+                Forms\Components\Group::make([
+                    Forms\Components\Section::make()
+                        ->schema([
+                            Forms\Components\Toggle::make('availability')
+                                ->label('Tersedia')
+                                ->required()
+                                ->inline(false),
+                        ]),
+                    Forms\Components\Section::make()
+                        ->schema([
+                            Forms\Components\TextInput::make('price')
+                                ->label(function (Get $get): string {
+                                    $isBeverage = $get('category') == MenuCategory::Beverage->value;
+                                    return $get('has_variant') && $isBeverage
+                                        ? 'Harga (Hot)'
+                                        : 'Harga';
+                                })
+                                ->required()
+                                ->numeric()
+                                ->mask(RawJs::make("\$money(\$input, ',', '.')"))
+                                ->stripCharacters('.')
+                                ->prefix('Rp')
+                                ->afterStateHydrated(function (Set $set, ?Menu $record, string $operation): void {
+                                    if ($operation === 'edit') {
+                                        $prices = $record->prices;
+                                        if ($record->prices->count() > 1) {
+                                            $set(
+                                                'price',
+                                                $prices->firstWhere(
+                                                    'variant_beverage',
+                                                    VariantBeverage::Hot
+                                                )->price
+                                            );
 
-                                $set(
-                                    'price_cold',
-                                    $prices->firstWhere(
-                                        'variant_beverage',
-                                        VariantBeverage::Cold
-                                    )->price
-                                );
-                            } else {
-                                $set('price', $prices->first()->price);
-                            }
-                        }
-                    }),
-                Forms\Components\TextInput::make('price_cold')
-                    ->label('Harga (Cold)')
-                    ->required()
-                    ->numeric()
-                    ->mask(RawJs::make("\$money(\$input, ',', '.')"))
-                    ->stripCharacters('.')
-                    ->prefix('Rp')
-                    ->visible(function (Get $get): bool {
-                        return $get('has_variant') && $get('category') == MenuCategory::Beverage->value;
-                    }),
-                Forms\Components\Toggle::make('availability')
-                    ->label('Tersedia')
-                    ->required()
-                    ->inline(false),
+                                            $set(
+                                                'price_cold',
+                                                $prices->firstWhere(
+                                                    'variant_beverage',
+                                                    VariantBeverage::Cold
+                                                )->price
+                                            );
+                                        } else {
+                                            $set('price', $prices->first()->price);
+                                        }
+                                    }
+                                }),
+                            Forms\Components\TextInput::make('price_cold')
+                                ->label('Harga (Cold)')
+                                ->required()
+                                ->numeric()
+                                ->mask(RawJs::make("\$money(\$input, ',', '.')"))
+                                ->stripCharacters('.')
+                                ->prefix('Rp')
+                                ->visible(function (Get $get): bool {
+                                    return $get('has_variant') && $get('category') == MenuCategory::Beverage->value;
+                                }),
+                        ])
+                ]),
             ]);
     }
 

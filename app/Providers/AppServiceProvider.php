9ee9003b5e01a62d9c3cc44f80e59afa7f96682d
Muhammad Akmal <86710767+maakmall@users.noreply.github.com>
2025-05-09 09:49:22 +0700

    Update AppServiceProvider.php storage path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        
+        if (app()->environment('production')) {
+            app()->useStoragePath('/tmp/storage');
+        }
     }
 
     /**
@@ -19,12 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-       if (app()->environment('production')) {
-            config([
-                'filesystems.disks.local.root' => '/tmp',
-                'view.compiled' => '/tmp/views',
-                'cache.stores.file.path' => '/tmp/cache',
-            ]);
-        }
+       // if (app()->environment('production')) {
+       //      config([
+       //          'filesystems.disks.local.root' => '/tmp',
+       //          'view.compiled' => '/tmp/views',
+       //          'cache.stores.file.path' => '/tmp/cache',
+       //      ]);
+       //  }
     }
 }

28684353414815150f590c35e537d491947736f4
Muhammad Akmal <86710767+maakmall@users.noreply.github.com>
2025-05-09 09:04:21 +0700

    Update AppServiceProvider.php wrong

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,13 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if (app()->environment('production')) {
-            config([
-                'filesystems.disks.local.root' => '/tmp',
-                'view.compiled' => '/tmp/views',
-                'cache.stores.file.path' => '/tmp/cache',
-            ]);
-        }
+        
     }
 
     /**
@@ -25,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       if (app()->environment('production')) {
+            config([
+                'filesystems.disks.local.root' => '/tmp',
+                'view.compiled' => '/tmp/views',
+                'cache.stores.file.path' => '/tmp/cache',
+            ]);
+        }
     }
 }
